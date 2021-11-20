6b910a8bdc89b3f5c36148751837267f6329df29
Sara Peksin <48366564+peksinsara@users.noreply.github.com>
2021-11-20 18:11:05 +0100

    code

---
+++ crack-password.py
@@ -0,0 +1,27 @@
+import itertools
+import string
+import re 
+
+def guess_password():    
+    chars = string.ascii_lowercase + string.digits
+    attempts = 0
+    for password_length in range(1, 9):
+        for guess in itertools.product(chars, repeat=password_length):
+            attempts += 1
+            guess = ''.join(guess)
+            if guess == userInput:
+                return 'Password is {}. Found in {} guesses.'.format(guess, attempts)
+            print(guess, attempts)
+            
+            
+
+userInput=input('Please enter password: ')  
+regex = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
+
+while (regex.search(userInput) != None): 
+    print('Text has special characters, please do not use them.')
+    userInput=input('Please enter password: ')   
+    if(regex.search(userInput) == None): 
+        print(guess_password()) 
+ 
+
